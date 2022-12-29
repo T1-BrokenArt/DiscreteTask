65cb028ff8b3963fc58b6209197219f594e5fae5
T1-BrokenArt <121640573+T1-BrokenArt@users.noreply.github.com>
2022-12-29 23:07:31 +0200

    Add files via upload

---
+++ Prime Num/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Let's Check Your Prime Numbers.\n\n");
+            Console.WriteLine("Pick Your First Number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pick Your Second Number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\nCongratulation We Find Your Prime Numbers.");
+            Console.Write("\nYour Prime Numbers is: \n");
+
+            for (int x = num1; x <= num2; x++)
+            {
+                int Bushido_Counter = 0;
+
+                for (int y = 2; y < x; y++)
+                {
+                    if (x % y == 0)
+                    {
+                        Bushido_Counter++;
+                        break;
+                    }
+                }
+
+                if (Bushido_Counter == 0 && x > 1)
+                    Console.WriteLine(". " + x);
+            }
+
+            Console.WriteLine("\nTHANK YOU.");
+            Console.ReadKey();
+        }
+    }
+}
